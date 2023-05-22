22849e22ab6a82645c0708226a47a5d0940ef909
ZoharManor <zoharmanor2004@gmail.com>
2023-05-22 20:27:57 +0300

    page3-TS

--- project/Page-2/dist/register.js
+++ project/Page-2/dist/register.js
@@ -1,7 +1,32 @@
-var x = 0;
-var Xi = /** @class */ (function () {
-    function Xi() {
-        this.one = 1;
+var UserData = /** @class */ (function () {
+    function UserData(uid, userName, password, address, licensePlate, creditCard) {
+        this.uid = uid;
+        this.userName = userName;
+        this.password = password;
+        this.address = address;
+        this.licensePlate = licensePlate;
+        this.creditCard = creditCard;
     }
-    return Xi;
+    return UserData;
 }());
+var userData = [];
+function addUser(event) {
+    event.preventDefault();
+    var userNameInput = document.getElementById("user-name");
+    var passwordInput = document.getElementById("password");
+    var addressInput = document.getElementById("address");
+    var licensePlateInput = document.getElementById("license-plate-number");
+    var creditCardInput = document.getElementById("credit-card");
+    var newUserData = {
+        uid: Math.floor(Math.random() * 10000),
+        userName: userNameInput.value,
+        password: passwordInput.value,
+        address: addressInput.value,
+        licensePlate: Number(licensePlateInput.value),
+        creditCard: Number(creditCardInput.value)
+    };
+    userData.push(newUserData);
+    console.log(userData);
+}
+var form = document.getElementById("form");
+form.addEventListener("submit", addUser);
--- project/Page-2/register.ts
+++ project/Page-2/register.ts
@@ -1,4 +1,41 @@
-const x: number = 0;
-class Xi {
-  public one: number = 1;
+class UserData {
+  constructor(
+    public uid: number,
+    public userName: string,
+    public password: string,
+    public address: string,
+    public licensePlate: number,
+    public creditCard: number
+  ) {}
 }
+
+const userData: UserData[] = [];
+
+function addUser(event: Event) {
+  event.preventDefault();
+  const userNameInput = document.getElementById(
+    "user-name"
+  ) as HTMLInputElement;
+  const passwordInput = document.getElementById("password") as HTMLInputElement;
+  const addressInput = document.getElementById("address") as HTMLInputElement;
+  const licensePlateInput = document.getElementById(
+    "license-plate-number"
+  ) as HTMLInputElement;
+  const creditCardInput = document.getElementById(
+    "credit-card"
+  ) as HTMLInputElement;
+
+  const newUserData: UserData = {
+    uid: Math.floor(Math.random() * 10000),
+    userName: userNameInput.value,
+    password: passwordInput.value,
+    address: addressInput.value,
+    licensePlate: Number(licensePlateInput.value),
+    creditCard: Number(creditCardInput.value),
+  };
+  userData.push(newUserData);
+  console.log(userData);
+}
+
+const form = document.getElementById("form") as HTMLFormElement;
+form.addEventListener("submit", addUser);
