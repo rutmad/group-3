e79be8c7508c4c66714d531787cfbfeb39a76258
rutmad <122039144+rutmad@users.noreply.github.com>
2023-05-23 22:44:03 +0300

    rut-basic

---
+++ project/page-3/dist/parking.js
@@ -0,0 +1,52 @@
+var citySelect = document.getElementById("city");
+var vehicleNumberInput = document.getElementById("number");
+var startParkingButton = document.getElementById("start-parking");
+var endParkingButton = document.getElementById("end-parking");
+var timerLabel = document.getElementById("timer-label");
+var timerDisplay = document.getElementById("timer");
+var timer;
+var seconds = 0;
+// Event listeners
+startParkingButton.addEventListener("click", startParking);
+endParkingButton.addEventListener("click", endParking);
+// Start parking
+function startParking() {
+    var selectedCity = citySelect.value;
+    var vehicleNumber = vehicleNumberInput.value;
+    if (selectedCity && vehicleNumber) {
+        // Disable city selection and vehicle number input
+        citySelect.disabled = true;
+        vehicleNumberInput.disabled = true;
+        // Enable end parking button
+        endParkingButton.disabled = false;
+        // Show timer
+        timerLabel.textContent = "Time elapsed:";
+        timerDisplay.textContent = "0 seconds";
+        if (timerDisplay.parentElement) {
+            timerDisplay.parentElement.hidden = false;
+        }
+        // Start timer
+        timer = setInterval(updateTimer, 1000);
+    }
+}
+// End parking
+function endParking() {
+    // Enable city selection and vehicle number input
+    citySelect.disabled = false;
+    vehicleNumberInput.disabled = false;
+    // Disable end parking button
+    endParkingButton.disabled = true;
+    // Hide timer
+    if (timerDisplay.parentElement) {
+        timerDisplay.parentElement.hidden = true;
+    }
+    // Stop timer
+    clearInterval(timer);
+    // Reset timer
+    seconds = 0;
+}
+// Update timer display
+function updateTimer() {
+    seconds++;
+    timerDisplay.textContent = seconds + " seconds";
+}
