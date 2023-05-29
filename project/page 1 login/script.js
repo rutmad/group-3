daf260970df4c4719c340f0772bd2e89993d9246
amirrahm123 <amirrahm135@gmail.com>
2023-05-29 18:45:01 +0300

    amir

--- project/page 1 login/script.js
+++
@@ -1,6 +0,0 @@
-const wrapper = document.querySelector(".wrapper");
-const loginLink = document.querySelector(".login-link");
-
-loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-});
