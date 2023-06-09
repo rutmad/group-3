463cdf77d2a2f18ce4bb3b64cc7e97f04d21ce2f
orshkolnik <orshkolnik1@gmail.com>
2023-06-09 12:59:12 +0300

    welcome page update

---
+++ project/or/dist/model.js
@@ -0,0 +1,65 @@
+var TimerModel = /** @class */ (function () {
+    function TimerModel(seconds, timer, onUpdateCallback) {
+        this.seconds = seconds;
+        this.timer = timer;
+        this.onUpdateCallback = onUpdateCallback;
+        this.seconds = 0;
+        this.timer = 0;
+        this.onUpdateCallback = function () { };
+    }
+    TimerModel.prototype.startTimer = function () {
+        var _this = this;
+        this.timer = setInterval(function () {
+            _this.seconds++;
+            _this.onUpdateCallback(_this.seconds);
+        }, 1000);
+    };
+    TimerModel.prototype.stopTimer = function () {
+        clearInterval(this.timer);
+    };
+    TimerModel.prototype.resetTimer = function () {
+        this.seconds = 0;
+        this.onUpdateCallback(this.seconds);
+    };
+    TimerModel.prototype.getSeconds = function () {
+        return this.seconds;
+    };
+    TimerModel.prototype.getSecondsElapsed = function () {
+        return this.seconds;
+    };
+    return TimerModel;
+}());
+var ParkingModel = /** @class */ (function () {
+    function ParkingModel(selectedCity, vehicleNumber) {
+        this.selectedCity = selectedCity;
+        this.vehicleNumber = vehicleNumber;
+        this.selectedCity = "";
+        this.vehicleNumber = "";
+    }
+    ParkingModel.prototype.setSelectedCity = function (city) {
+        this.selectedCity = city;
+    };
+    ParkingModel.prototype.setVehicleNumber = function (number) {
+        this.vehicleNumber = number;
+    };
+    ParkingModel.prototype.getSelectedCity = function () {
+        return this.selectedCity;
+    };
+    ParkingModel.prototype.getVehicleNumber = function () {
+        return this.vehicleNumber;
+    };
+    return ParkingModel;
+}());
+//////////////Register/////////////////
+var UserData = /** @class */ (function () {
+    function UserData(uid, userName, password, address, licensePlate, creditCard) {
+        this.uid = uid;
+        this.userName = userName;
+        this.password = password;
+        this.address = address;
+        this.licensePlate = licensePlate;
+        this.creditCard = creditCard;
+    }
+    return UserData;
+}());
+var userData = [];
---
+++ project/or/index.ts
@@ -0,0 +1,1 @@
+const parkingController = new ParkingController();
